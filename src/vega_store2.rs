41b3156ea5782da20285d2b19fed8d04c6e6fa8d
Jeremy Letang <me@jeremyletang.com>
2023-12-20 10:02:59 +0100

    feat: listen to trades + keep track of them
    Signed-off-by: Jeremy Letang <me@jeremyletang.com>

--- src/vega_store2.rs
+++ src/vega_store2.rs
@@ -1,4 +1,5 @@
 use log::{error, info};
+use lru::LruCache;
 use std::collections::HashMap;
 use std::error::Error as StdError;
 use std::fmt;
@@ -8,17 +9,30 @@ use tonic;
 use vega_protobufs::datanode::api::v2::GetLatestMarketDataRequest;
 use vega_protobufs::vega::MarketData;
 
+const TRADES_CACHE: usize = 10000;
+
 use vega_protobufs::{
     datanode::api::v2::{
         trading_data_service_client::TradingDataServiceClient, GetMarketRequest, ListAssetsRequest,
-        ObserveMarketsDataRequest, ObservePositionsRequest,
+        ObserveMarketsDataRequest, ObservePositionsRequest, ObserveTradesRequest,
     },
-    vega::{Asset, Market, Position},
+    vega::{Asset, Market, Position, Trade},
 };
+
+#[derive(Clone)]
+pub struct TradeStat {
+    pub timestamp: i64,
+    pub price: String,
+    pub size: u64,
+    pub block_best_bid: Option<String>,
+    pub block_best_ask: Option<String>,
+}
 
 pub struct VegaStore {
     market: Market,
     market_data: MarketData,
+    previous_market_data: LruCache<i64, MarketData>,
+    trades: Vec<TradeStat>,
     positions: HashMap<String, Position>,
     // key = asset ID
     assets: HashMap<String, Asset>,
@@ -46,7 +60,7 @@ impl VegaStore {
             .await?;
 
         let market_data = mktd_resp.get_ref().market_data.as_ref().unwrap().clone();
-        info!("market data found: {:?}", market,);
+        info!("market data found: {:?}", market);
 
         let assets_resp = clt
             .list_assets(ListAssetsRequest {
@@ -63,11 +77,17 @@ impl VegaStore {
 
         let positions: HashMap<String, Position> = HashMap::new();
 
+        let mut previous_market_data: LruCache<i64, MarketData> =
+            LruCache::new(core::num::NonZeroUsize::new(1000).unwrap());
+
+        previous_market_data.put(market_data.timestamp, market_data.clone());
         return Ok(VegaStore {
-            market_data,
             market,
+            market_data,
+            previous_market_data,
             assets,
             positions,
+            trades: vec![],
         });
     }
 
@@ -98,7 +118,43 @@ impl VegaStore {
     }
 
     pub fn save_market_data(&mut self, md: MarketData) {
+        self.update_trades(&md);
+        self.previous_market_data.put(md.timestamp, md.clone());
         self.market_data = md
+    }
+
+    pub fn update_trades(&mut self, md: &MarketData) {
+        for t in self.trades.iter_mut() {
+            if t.timestamp == md.timestamp {
+                t.block_best_ask = Some(md.best_offer_price.clone());
+                t.block_best_bid = Some(md.best_bid_price.clone());
+            }
+        }
+    }
+
+    pub fn save_trade(&mut self, trade: &Trade) {
+        if self.trades.len() > TRADES_CACHE {
+            self.trades.remove(0);
+        }
+
+        let (best_bid, best_ask) = match self.previous_market_data.get(&trade.timestamp) {
+            Some(md) => (
+                Some(md.best_bid_price.clone()),
+                Some(md.best_offer_price.clone()),
+            ),
+            None => (None, None),
+        };
+        self.trades.push(TradeStat {
+            timestamp: trade.timestamp,
+            price: trade.price.clone(),
+            size: trade.size,
+            block_best_ask: best_ask,
+            block_best_bid: best_bid,
+        });
+    }
+
+    pub fn get_trades(&self) -> Vec<TradeStat> {
+        return self.trades.clone();
     }
 }
 
@@ -114,6 +170,12 @@ pub fn update_forever(
         market.to_string(),
     ));
     tokio::spawn(update_position_forever(
+        store.clone(),
+        clt.clone(),
+        market.to_string(),
+        pubkey1.to_string(),
+    ));
+    tokio::spawn(update_trades_forever(
         store.clone(),
         clt.clone(),
         market.to_string(),
@@ -184,6 +246,38 @@ async fn update_position_forever(
                 },
                 _ => {}
             },
+            Err(e) => {
+                error!("could not load position: {} - {}", e, e.message());
+            }
+        }
+    }
+}
+
+async fn update_trades_forever(
+    store: Arc<Mutex<VegaStore>>,
+    mut clt: TradingDataServiceClient<tonic::transport::Channel>,
+    market: String,
+    pubkey: String,
+) {
+    info!("starting positions stream for party: {}...", &*pubkey);
+    let mut stream = match clt
+        .observe_trades(ObserveTradesRequest {
+            party_ids: vec![pubkey],
+            market_ids: vec![market],
+        })
+        .await
+    {
+        Ok(s) => s.into_inner(),
+        Err(e) => panic!("{:?}", e),
+    };
+
+    while let Some(item) = stream.next().await {
+        match item {
+            Ok(resp) => {
+                for t in resp.trades.iter() {
+                    store.lock().unwrap().save_trade(t);
+                }
+            }
             Err(e) => {
                 error!("could not load position: {} - {}", e, e.message());
             }
